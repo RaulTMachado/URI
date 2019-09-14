e65c8a8258c22d316227e4605787d267e9ddcecb
Raul Teodoro <raul.tmachado2@gmail.com>
2019-09-14 13:24:21 -0300

    exercise number 1001 = firsti exercise

---
+++ URI_1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+int main(){
+    int A=0,B=0;
+    scanf("%d", &A);
+    scanf("%d", &B);
+    int X = A+B;
+    printf("X = %d \n", X);
+
+    return 0;
+
+
+}
